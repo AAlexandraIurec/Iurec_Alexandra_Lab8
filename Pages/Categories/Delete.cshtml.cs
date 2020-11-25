389513578cafecd345f6b72fbd7b9487cb3e5883
Alexandra <alexandraiurec@yahoo.com>
2020-11-25 17:50:34 +0200

    Add project files.

---
+++ Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Iurec_Alexandra_Lab8.Data;
+using Iurec_Alexandra_Lab8.Models;
+
+namespace Iurec_Alexandra_Lab8.Pages.Categories
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Iurec_Alexandra_Lab8.Data.Iurec_Alexandra_Lab8Context _context;
+
+        public DeleteModel(Iurec_Alexandra_Lab8.Data.Iurec_Alexandra_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category = await _context.Category.FindAsync(id);
+
+            if (Category != null)
+            {
+                _context.Category.Remove(Category);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
